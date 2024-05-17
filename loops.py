7783b22ad6f7ad66847e22a209cc623c32489e75
anasixto <162721927+anasixto@users.noreply.github.com>
2024-05-17 19:15:51 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,40 +1,47 @@
-def index_of_by_index(word, list_1, index):
-    if index < 0 or index >= len(list_1):
-        return -1
-        
-    for index in range(index, len(list_1)):
-        if list_1[index] == word:
-            return index
+def index_of_by_index(word, my_list, index):
+    i = index
+    while i < len(my_list):
+        if my_list[i] == word:
+            return i
+        i += 1
     return -1
 
 
-def index_of_empty(list_1):
-    for index, value in enumerate(list_1):
-        if value == "":
-            return index
+def index_of_empty(my_list):
+    i = 0
+    while i < len(my_list):
+        if my_list[i] == "":
+            return i
+        i += 1
     return -1
     
 
 
-def index_of(word, list_1):
-    for index, value in enumerate(list_1):
-        if value == word:
-            return index
+def index_of(word, my_list):
+    i = 0
+    while i < len(my_list):
+        if my_list[i] == word:
+            return i
+        i += 1
     return -1
 
 
-def put(word, list_1):
-    for index, value in enumerate(list_1):
-        if value == "":
-            list_1[index] = word
-            return index
+def put(word, my_list):
+    i = 0
+    while i < len(my_list):
+        if my_list[i] == "":
+            my_list[i] = word
+            return i
+        i += 1
     return -1
 
 
-def remove(word, list):
+def remove(word, my_list):
+    i = 0
     count = 0
-    for index, value in enumerate(list_1):
-        if value == word:
-            list_1[index] = ""
+    while i < len(my_list):
+        if my_list[i] == word:
+            my_list[i] = ""
             count += 1
+        i += 1
     return count
