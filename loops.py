4b5d5552dec40e2c264892214d69243382519c2f
anasixto <162721927+anasixto@users.noreply.github.com>
2024-05-17 17:03:15 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,36 +1,32 @@
 def index_of_by_index(word, list_1, index):
-    for index, value in enumerate(list_1):
-        if value == word:
+    if index < 0 or index >= len(list_1):
+        return -1
+        
+    for index in range(index, len(list_1)):
+        if list_1[index] == word:
             return index
     return -1
 
 
-def index_of_empty(target, list_1, start_index):
-    # Aseguramos que el indice de inicio este dentro del rango valido
-    if start_index < 0 or start_index >= len(list_1):
-        return -1
-    # Recorremos la lista desde el indice de inicio hasta el final
-    for index in range(start_index, len(list_1)):
-        if list_1[index] == target:
+def index_of_empty(list_1):
+    for index, value in enumerate(list_1):
+        if value == "":
             return index
     return -1
     
 
 
 def index_of(word, list_1):
-    # Recorremos la lista con un bulce
     for index, value in enumerate(list_1):
-        # Si encontramos un elemento vacio, retornamos el indice
-        if index == "":
+        if value == word:
             return index
-    # Si no encontramos ningun elemento vacio, retornamos -1
     return -1
 
 
 def put(word, list_1):
     for index, value in enumerate(list_1):
         if value == "":
-            list_1 = word
+            list_1[index] = word
             return index
     return -1
 
@@ -40,4 +36,5 @@ def remove(word, list):
     for index, value in enumerate(list_1):
         if value == word:
             list_1[index] = ""
-    return -1
+            count += 1
+    return count
