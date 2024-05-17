9121e606383f659238bd8d0a79a5913477e47a4b
anasixto <162721927+anasixto@users.noreply.github.com>
2024-05-17 19:27:09 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,47 +1,46 @@
 def index_of_by_index(word, my_list, index):
-    i = index
+    i=index
     while i < len(my_list):
-        if my_list[i] == word:
+        if my_list[i]==word:
             return i
-        i += 1
+        i+=1
     return -1
 
 
 def index_of_empty(my_list):
-    i = 0
+    i=0
     while i < len(my_list):
         if my_list[i] == "":
             return i
         i += 1
     return -1
-    
 
 
 def index_of(word, my_list):
-    i = 0
+    i=0
     while i < len(my_list):
-        if my_list[i] == word:
-            return i
-        i += 1
+        if my_list[i]==word:
+            return i 
+        i+=1
     return -1
 
 
 def put(word, my_list):
-    i = 0
+    i=0
     while i < len(my_list):
-        if my_list[i] == "":
-            my_list[i] = word
+        if my_list[i]=="":
+            my_list[i]= word
             return i
-        i += 1
+        i+=1
     return -1
 
 
 def remove(word, my_list):
-    i = 0
-    count = 0
+    i=0
+    count=0
     while i < len(my_list):
-        if my_list[i] == word:
-            my_list[i] = ""
-            count += 1
-        i += 1
+        if my_list[i]==word:
+            my_list[i]=""
+            count+=1
+        i+=1
     return count
