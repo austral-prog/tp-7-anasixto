3ff4d907d9e97f29d02eb9686460efde8197bcad
anasixto <162721927+anasixto@users.noreply.github.com>
2024-05-17 19:27:04 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,28 +1,22 @@
 def enumerate_list(my_list):
-    # list_2 es para guardar todos los elementos que no sean vacios y su numero (ej.: 0. Black, 1. White, etc.)
-    new_list = []
-    # current_index es para recorrer la list_1, va recorriendo cada uno de sus elementos, y para ir al siguiente elemento le tenemos que aumentar su valor en 1
-    current_index = 0
-    # number es para mantener la cuenta de la cantidad de elementos NO vacios de la list_1, y despues ese numero lo usamos cuando escribimos el numero en la list_2. Solo aumentamos su valor cuando el elemento no es vacio!
-    number = 0
+    new_list=[]
+    current_index= 0
+    number=0 
     while current_index < len(my_list):
-        if my_list[current_index] != "":
-            new_list.append (f"{number}. {my_list[current_index]}")
-            number += 1
-        current_index += 1
+        if my_list[current_index]!= "": 
+            new_list.append(f"{number}. {my_list[current_index]}")
+            number+=1
+        current_index+=1
     return new_list
 
 
 def enumerate_backwards(my_list):
-    # list_2 es para guardar todos los elementos que no sean vacios y su numero (ej.: 0. Black, 1. White, etc.)
-    new_list = []
-    # current_index es para recorrer la list_1, va recorriendo cada uno de sus elementos, y para ir al siguiente elemento le tenemos que aumentar su valor en 1
-    current_index = 0
-    # number es para mantener la cuenta de la cantidad de elementos NO vacios de la list_1, y despues ese numero lo usamos cuando escribimos el numero en la list_2. Solo aumentamos su valor cuando el elemento no es vacio!
-    number = 0
+    new_list=[]
+    current_index= 0
+    number=0 
     while current_index < len(my_list):
-        if my_list[current_index] != "":
+        if my_list[current_index]!= "": 
             new_list.append(f"{number}. {my_list[current_index][::-1]}")
-            number += 1
-        current_index += 1
+            number+=1
+        current_index+=1
     return new_list
